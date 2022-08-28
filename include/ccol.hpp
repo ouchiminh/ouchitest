16f6b61fd26adf75af42eb96f2a19413db46186d
ouchiminh <39828763+ouchiminh@users.noreply.github.com>
2022-08-28 21:52:37 +0900

    Add files via upload

--- include/ccol.hpp
+++ include/ccol.hpp
@@ -1,8 +1,39 @@
 ﻿#pragma once
 
-#if !defined(__linux__)
-    // impl for windows
-#   include <Windows.h>
+#ifdef __linux__
+#include <cstdio>
+namespace ouchi::test {
+
+class ccol {
+public:
+    enum color_code : unsigned short {
+        foreground_blue = 34,
+        foreground_green = 32,
+        foreground_red = 31,
+        background_blue = 44,
+        background_green = 42,
+        background_red = 41,
+    };
+    friend color_code operator|(color_code c1, color_code c2) noexcept { return (color_code)((unsigned short)c1 | (unsigned short)c2); }
+    ccol() = default;
+    ccol(const ccol&) = delete;
+    ~ccol()
+    {
+        restore();
+    }
+    void set_color(color_code c) const noexcept
+    {
+        std::printf("\x1b[%dm", c);
+    }
+    void restore() const noexcept
+    {
+        std::printf("\x1b[0m");
+    }
+};
+}
+#else
+// impl for windows
+#include <Windows.h>
 
 namespace ouchi::test {
 
@@ -39,37 +70,6 @@ public:
     void restore() const noexcept
     {
         SetConsoleTextAttribute(stdout_, csbi_.wAttributes);
-    }
-};
-}
-#else
-#include <cstdio>
-namespace ouchi::test {
-
-class ccol {
-public:
-    enum color_code : WORD {
-        foreground_blue = 34,
-        foreground_green = 32,
-        foreground_red = 31,
-        background_blue = 44,
-        background_green = 42,
-        background_red = 41,
-    };
-    friend color_code operator|(color_code c1, color_code c2) noexcept { return (color_code)((WORD)c1 | (WORD)c2); }
-    ccol() = default;
-    ccol(const ccol&) = delete;
-    ~ccol()
-    {
-        restore();
-    }
-    void set_color(color_code c) const noexcept
-    {
-        std::printf("\x1b[%dm", c)
-    }
-    void restore() const noexcept
-    {
-        std::printf("\x1b[0m")
     }
 };
 }
